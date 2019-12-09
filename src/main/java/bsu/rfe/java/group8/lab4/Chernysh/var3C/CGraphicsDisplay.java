26a754b7e56c7accc3f13954a1c799b57a4403bc
Geradott <ic2ilya@gmail.com>
2019-12-09 09:42:17 +0300

    Base program

---
+++ src/main/java/bsu/rfe/java/group8/lab4/Chernysh/var3C/CGraphicsDisplay.java
@@ -0,0 +1,210 @@
+package bsu.rfe.java.group8.lab4.Chernysh.var3C;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Stroke;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.*;
+import javax.swing.JPanel;
+
+public class CGraphicsDisplay extends JPanel {
+    private Double[][] dArrGraphicsData;
+    private boolean bShowAxis = true;
+    private boolean bShowMarkers = true;
+    private boolean bClockRotate = false;
+    private double dMinX;
+    private double dMaxX;
+    private double dMinY;
+    private double dMaxY;
+    private double dScale;
+    private BasicStroke graphicsStroke;
+    private BasicStroke axisStroke;
+    private BasicStroke markerStroke;
+    private Font axisFont;
+
+    CGraphicsDisplay() {
+        setBackground(Color.WHITE);
+        graphicsStroke = new BasicStroke(5.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_MITER, 10.0f, new float[] {6,2,6,2,6,2,2,2,2,2,2,2}, 0.0f);
+        axisStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
+        markerStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
+        axisFont = new Font("Serif", Font.BOLD, 36);
+    }
+
+    void showGraphics(Double[][] graphicsData) {
+        this.dArrGraphicsData = graphicsData;
+        repaint();
+    }
+
+    void setShowAxis(boolean showAxis) {
+        this.bShowAxis = showAxis;
+        repaint();
+    }
+
+    void setShowMarkers(boolean showMarkers) {
+        this.bShowMarkers = showMarkers;
+        repaint();
+    }
+
+    void setClockRotate(boolean clockRotate) {
+        this.bClockRotate = clockRotate;
+        repaint();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (dArrGraphicsData == null || dArrGraphicsData.length == 0)
+            return;
+        dMinX = dArrGraphicsData[0][0];
+        dMaxX = dArrGraphicsData[dArrGraphicsData.length - 1][0];
+        dMinY = dArrGraphicsData[0][1];
+        dMaxY = dMinY;
+
+        for (int i = 1; i < dArrGraphicsData.length; i++) {
+            if (dArrGraphicsData[i][1] < dMinY) {
+                dMinY = dArrGraphicsData[i][1];
+            }
+            if (dArrGraphicsData[i][1] > dMaxY) {
+                dMaxY = dArrGraphicsData[i][1];
+            }
+        }
+
+        double dScaleX = getSize().getWidth() / (dMaxX - dMinX);
+        double dScaleY = getSize().getHeight() / (dMaxY - dMinY);
+        dScale = Math.min(dScaleX, dScaleY);
+        if (dScale == dScaleX) {
+            double yIncrement = (getSize().getHeight() / dScale - (dMaxY - dScale)) / 2;
+            dMaxY += yIncrement;
+            dMinY -= yIncrement;
+        }
+        if (dScale == dScaleY) {
+            double xIncrement = (getSize().getWidth() / dScale - (dMaxX - dMinX)) / 2;
+            dMaxX += xIncrement;
+            dMinX -= xIncrement;
+        }
+
+        Graphics2D canvas = (Graphics2D) g;
+        Stroke oldStroke = canvas.getStroke();
+        Color oldColor = canvas.getColor();
+        Paint oldPaint = canvas.getPaint();
+        Font oldFont = canvas.getFont();
+
+        if (bClockRotate) {
+            paintRotate(canvas);
+        }
+        if (bShowAxis)
+            paintAxis(canvas);
+        paintGraphics(canvas);
+        if (bShowMarkers)
+            paintMarkers(canvas);
+
+        canvas.setFont(oldFont);
+        canvas.setPaint(oldPaint);
+        canvas.setColor(oldColor);
+        canvas.setStroke(oldStroke);
+    }
+
+    private void paintGraphics(Graphics2D canvas) {
+        canvas.setStroke(graphicsStroke);
+        canvas.setColor(Color.RED);
+        GeneralPath graphics = new GeneralPath();
+        for (int i = 0; i < dArrGraphicsData.length; i++) {
+            Point2D.Double point = xyToPoint(dArrGraphicsData[i][0], dArrGraphicsData[i][1]);
+            if (i > 0) {
+                graphics.lineTo(point.getX(), point.getY());
+            } else {
+                graphics.moveTo(point.getX(), point.getY());
+            }
+        }
+        canvas.draw(graphics);
+    }
+
+    private void paintRotate(Graphics2D canvas) {
+    }
+
+    private void paintMarkers(Graphics2D canvas) {
+
+        canvas.setStroke(markerStroke);
+        for (Double[] point : dArrGraphicsData) {
+            canvas.setStroke(axisStroke);
+            int res = Math.abs(point[1].intValue()), sum = 0;
+            while (res > 0) {
+                sum += res % 10;
+                res = res / 10;
+            }
+            if (sum < 10) {
+                canvas.setColor(Color.BLACK);
+                canvas.setPaint(Color.BLACK);
+            } else {
+                canvas.setColor(Color.RED);
+                canvas.setPaint(Color.RED);
+            }
+            Ellipse2D.Double marker = new Ellipse2D.Double();
+            Point2D.Double center = xyToPoint(point[0], point[1]);
+            Point2D.Double corner = shiftPoint(center, 5, 5);
+            marker.setFrameFromCenter(center, corner);
+            canvas.draw(marker);
+            canvas.draw(new Line2D.Double(shiftPoint(center, 0, 5), shiftPoint(center, 0, -5)));
+            canvas.draw(new Line2D.Double(shiftPoint(center, 5, 0), shiftPoint(center, -5, 0)));
+        }
+    }
+
+
+    private void paintAxis(Graphics2D canvas) {
+        canvas.setStroke(axisStroke);
+        canvas.setColor(Color.BLACK);
+        canvas.setPaint(Color.BLACK);
+        canvas.setFont(axisFont);
+
+        FontRenderContext context = canvas.getFontRenderContext();
+        if (dMinX <= 0.0 && dMaxX >= 0.0) {
+            canvas.draw(new Line2D.Double(xyToPoint(0, dMaxY), xyToPoint(0, dMinY)));
+            GeneralPath arrow = new GeneralPath();
+            Point2D.Double lineEnd = xyToPoint(0, dMaxY);
+            arrow.moveTo(lineEnd.getX(), lineEnd.getY());
+            arrow.lineTo(arrow.getCurrentPoint().getX() + 5, arrow.getCurrentPoint().getY() + 20);
+            arrow.lineTo(arrow.getCurrentPoint().getX() - 10, arrow.getCurrentPoint().getY());
+            arrow.closePath();
+            canvas.draw(arrow);
+            canvas.fill(arrow);
+            Rectangle2D bounds = axisFont.getStringBounds("y", context);
+            Point2D.Double labelPos = xyToPoint(0, dMaxY);
+            canvas.drawString("y", (float) labelPos.getX() + 10, (float) (labelPos.getY() - bounds.getY()));
+        }
+
+        if (dMinY <= 0.0 && dMaxY >= 0.0) {
+            canvas.draw(new Line2D.Double(xyToPoint(dMinX, 0), xyToPoint(dMaxX, 0)));
+            GeneralPath arrow = new GeneralPath();
+            Point2D.Double lineEnd = xyToPoint(dMaxX, 0);
+            arrow.moveTo(lineEnd.getX(), lineEnd.getY());
+            arrow.lineTo(arrow.getCurrentPoint().getX() - 20, arrow.getCurrentPoint().getY() - 5);
+            arrow.lineTo(arrow.getCurrentPoint().getX(), arrow.getCurrentPoint().getY() + 10);
+            arrow.closePath();
+            canvas.draw(arrow);
+            canvas.fill(arrow);
+
+            Rectangle2D bounds = axisFont.getStringBounds("x", context);
+            Point2D.Double labelPos = xyToPoint(dMaxX, 0);
+            canvas.drawString("x", (float) (labelPos.getX() - bounds.getWidth() - 10), (float) (labelPos.getY() + bounds.getY()));
+        }
+    }
+
+    private Point2D.Double xyToPoint(double x, double y) {
+        double deltaX = x - dMinX;
+        double deltaY = dMaxY - y;
+        return new Point2D.Double(deltaX * dScale, deltaY * dScale);
+    }
+
+    private Point2D.Double shiftPoint(Point2D.Double src, double deltaX, double deltaY) {
+        Point2D.Double dest = new Point2D.Double();
+        dest.setLocation(src.getX() + deltaX, src.getY() + deltaY);
+        return dest;
+    }
+}
+
---
+++ src/main/java/bsu/rfe/java/group8/lab4/Chernysh/var3C/CMainFrame.java
@@ -0,0 +1,122 @@
+package bsu.rfe.java.group8.lab4.Chernysh.var3C;
+
+import java.awt.BorderLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+
+public class CMainFrame extends JFrame {
+    private static final int iFWidth = 1080;
+    private static final int iFHeight = 720;
+    private JFileChooser fileChooser = null;
+    private JCheckBoxMenuItem showAxisMenuItem;
+    private JCheckBoxMenuItem showMarkersMenuItem;
+    private JCheckBoxMenuItem rotatesMenuItem;
+    private CGraphicsDisplay display = new CGraphicsDisplay();
+    private boolean fileLoaded = false;
+
+    private CMainFrame() {
+        super("Plotting graphs");
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        setLocation((kit.getScreenSize().width - iFWidth) / 2, (kit.getScreenSize().height - iFHeight) / 2);
+        setExtendedState(MAXIMIZED_BOTH);
+        
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        JMenu fileMenu = new JMenu("File");
+        menuBar.add(fileMenu);
+        Action openGraphicsAction = new AbstractAction("Open file with graph") {
+            public void actionPerformed(ActionEvent event) {
+                if (fileChooser == null) {
+                    fileChooser = new JFileChooser();
+                    fileChooser.setCurrentDirectory(new File("."));
+                }
+                if (fileChooser.showOpenDialog(CMainFrame.this) == JFileChooser.APPROVE_OPTION)
+                    openGraphics(fileChooser.getSelectedFile());
+            }
+         };
+        fileMenu.add(openGraphicsAction);
+
+        JMenu graphicsMenu = new JMenu("Graph");
+        menuBar.add(graphicsMenu);
+         Action showAxisAction = new AbstractAction("Show axes") {
+            public void actionPerformed(ActionEvent event) {
+                display.setShowAxis(showAxisMenuItem.isSelected());
+            }
+        };
+        showAxisMenuItem = new JCheckBoxMenuItem(showAxisAction);
+        graphicsMenu.add(showAxisMenuItem);
+        showAxisMenuItem.setSelected(true);
+
+        Action showMarkersAction = new AbstractAction("Show points") {
+            public void actionPerformed(ActionEvent event) {
+                display.setShowMarkers(showMarkersMenuItem.isSelected());
+            }
+        };
+        showMarkersMenuItem = new JCheckBoxMenuItem(showMarkersAction);
+        graphicsMenu.add(showMarkersMenuItem);
+        showMarkersMenuItem.setSelected(true);
+
+        Action rotatesShapeClockAction = new AbstractAction("Rotate") {
+            public void actionPerformed(ActionEvent event) {
+                display.setClockRotate(rotatesMenuItem.isSelected());
+            }
+        };
+        rotatesMenuItem = new JCheckBoxMenuItem(rotatesShapeClockAction);
+        graphicsMenu.add(rotatesMenuItem);
+        rotatesMenuItem.setSelected(false);
+        
+        graphicsMenu.addMenuListener(new GraphicsMenuListener());
+        getContentPane().add(display, BorderLayout.CENTER);
+        }
+
+    private void openGraphics(File selectedFile) {
+        try {
+            DataInputStream in = new DataInputStream(new FileInputStream(selectedFile));
+            Double[][] graphicsData = new Double[in.available() / (Double.SIZE / 8) / 2][];
+
+            int i = 0;
+            while (in.available() > 0) {
+                double x = in.readDouble();
+                double y = in.readDouble();
+                graphicsData[i++] = new Double[] {x, y};
+            }
+            if (graphicsData != null && graphicsData.length > 0) {
+                fileLoaded = true;
+                display.showGraphics(graphicsData);
+            }
+            in.close();
+        } catch (FileNotFoundException ex) {
+            JOptionPane.showMessageDialog(CMainFrame.this, "File don't exist", "Loading error", JOptionPane.WARNING_MESSAGE);
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(CMainFrame.this, "Reading points error", "Loading error", JOptionPane.WARNING_MESSAGE);
+        }
+    }
+
+    public static void main(String[] args) {
+        CMainFrame frame = new CMainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    private class GraphicsMenuListener implements MenuListener {
+        public void menuSelected(MenuEvent e) {
+            showAxisMenuItem.setEnabled(fileLoaded);
+            showMarkersMenuItem.setEnabled(fileLoaded);
+            rotatesMenuItem.setEnabled(fileLoaded);
+        }
+        public void menuDeselected(MenuEvent e) {
+        }
+        public void menuCanceled(MenuEvent e) {
+        }
+    }
+}
